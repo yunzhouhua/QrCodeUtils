882b93261927a6b403c0e1a2fba727afeed57ff1
华云舟 <huayunzhou@fclassroom.com>
2017-10-17 11:27:38 +0800

    二维码扫描修改

--- app/src/main/java/com/yunzhou/qrcodeutils/MainActivity.java
+++ app/src/main/java/com/yunzhou/qrcodeutils/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
--- qrcodelib/src/main/java/com/yunzhou/qrcodelib/zxing/activity/CaptureActivity.java
+++ qrcodelib/src/main/java/com/yunzhou/qrcodelib/zxing/activity/CaptureActivity.java
@@ -19,9 +19,14 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -33,17 +38,14 @@ import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.zxing.Result;
 import com.yunzhou.qrcodelib.R;
 import com.yunzhou.qrcodelib.zxing.camera.CameraManager;
-import com.yunzhou.qrcodelib.zxing.decode.DecodeBitmap;
 import com.yunzhou.qrcodelib.zxing.decode.DecodeThread;
 import com.yunzhou.qrcodelib.zxing.utils.BeepManager;
 import com.yunzhou.qrcodelib.zxing.utils.CaptureActivityHandler;
 import com.yunzhou.qrcodelib.zxing.utils.InactivityTimer;
-import com.yunzhou.qrcodelib.zxing.utils.SelectAlbumUtils;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -63,22 +65,42 @@ public class CaptureActivity extends Activity implements
 
     private static final String TAG = CaptureActivity.class.getSimpleName();
     public static final int RESULT_MULLT = 5;
+    private static final int CODE_GALLERY_REQUEST = 101;
+
+    public static final String SCAN_TITLE = "scan_title";
+    public static final String SCAN_TITLE_COLOR = "scan_title_color";
+    public static final String SCAN_TITLE_BG_COLOR = "scan_title_bg_color";
+    public static final String SCAN_DIST_COLOR = "scan_dist_color";
+    public static final String SCAN_DESCRIBE = "scan_describe";
+    public static final String SCAN_DESCRIBE_COLOR = "scan_describe_color";
+    public static final String SCAN_NEED_BEEP = "scan_need_beep";
+
+    private RelativeLayout mTitleBarView = null;
+    private TextView mTitleView = null;
+    private SurfaceView scanPreview = null;
+    private RelativeLayout scanContainer = null;
+    private RelativeLayout scanCropView = null;
+    private TextView mScanDescribeView = null;
+    private ImageView scanLine = null;
+    private ImageView ivBack = null;
+
     private CameraManager cameraManager;
     private CaptureActivityHandler handler;
     private InactivityTimer inactivityTimer;
     private BeepManager beepManager;
 
-    private SurfaceView scanPreview = null;
-    private RelativeLayout scanContainer;
-    private RelativeLayout scanCropView;
-    private ImageView scanLine;
+    //View 配置相关
+    private String mTitle;
+    private int mTitleColor;
+    private int mTitleBgColor;
+    private int mScanColor;
+    private String mScanDescribe;
+    private int mScanDescColor;
+    private boolean mNeedBeep;
+
 
     private Rect mCropRect = null;
     private boolean isHasSurface = false;
-    private ImageView ivBack, ivMullt;
-    private int captureType = 0;
-    private TextView tvAlbum;
-    private static final int CODE_GALLERY_REQUEST = 101;
 
     public Handler getHandler() {
         return handler;
@@ -88,8 +110,6 @@ public class CaptureActivity extends Activity implements
         return cameraManager;
     }
 
-    private DialogInterface.OnClickListener mOnClickListener;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -97,38 +117,66 @@ public class CaptureActivity extends Activity implements
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_capture);
-        captureType = getIntent().getIntExtra("type", 0);
-        String string = getApplication().getResources().getString(R.string.jwstr_scan_it);
-//        Log.e("hdltag", "onCreate(CaptureActivity.java:102):" +string);
-        scanPreview = (SurfaceView) findViewById(R.id.capture_preview);
-        scanContainer = (RelativeLayout) findViewById(R.id.capture_container);
-        scanCropView = (RelativeLayout) findViewById(R.id.capture_crop_view);
-        scanLine = (ImageView) findViewById(R.id.capture_scan_line);
-        ivBack = (ImageView) findViewById(R.id.iv_back);
-        ivMullt = (ImageView) findViewById(R.id.iv_mudle);
-        tvAlbum = (TextView) findViewById(R.id.tv_capture_select_album_jwsd);
+        initData();
+        initViews();
+
         ivBack.setTag(123);
-        ivMullt.setTag(124);
-        tvAlbum.setTag(125);
-        tvAlbum.setOnClickListener(this);
         ivBack.setOnClickListener(this);
-        ivMullt.setOnClickListener(this);
         inactivityTimer = new InactivityTimer(this);
         beepManager = new BeepManager(this);
-
-        if (captureType == 1) {
-            ivMullt.setVisibility(View.INVISIBLE);
-        }
 
         TranslateAnimation animation = new TranslateAnimation(
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
                 Animation.RELATIVE_TO_PARENT, 0.0f,
-                Animation.RELATIVE_TO_PARENT, 0.9f);
+                Animation.RELATIVE_TO_PARENT, 1.0f);
         animation.setDuration(4500);
         animation.setRepeatCount(-1);
         animation.setRepeatMode(Animation.RESTART);
         scanLine.startAnimation(animation);
+    }
+
+    private void initData() {
+        mTitle = getIntent().getStringExtra(SCAN_TITLE);
+        mTitleColor = getIntent().getIntExtra(SCAN_TITLE_COLOR, Color.WHITE);
+        mTitleBgColor = getIntent().getIntExtra(SCAN_TITLE_BG_COLOR, Color.BLACK);
+        mScanColor = getIntent().getIntExtra(SCAN_DIST_COLOR, Color.parseColor("#65e102"));
+        mScanDescribe = getIntent().getStringExtra(SCAN_DESCRIBE);
+        mScanDescColor = getIntent().getIntExtra(SCAN_DESCRIBE_COLOR, Color.WHITE);
+        mNeedBeep = getIntent().getBooleanExtra(SCAN_NEED_BEEP, false);
+    }
+
+    private void initViews() {
+        mTitleBarView = (RelativeLayout) findViewById(R.id.scan_title_bar);
+        mTitleView = (TextView) findViewById(R.id.scan_title);
+        scanPreview = (SurfaceView) findViewById(R.id.capture_preview);
+        scanContainer = (RelativeLayout) findViewById(R.id.capture_container);
+        scanCropView = (RelativeLayout) findViewById(R.id.capture_crop_view);
+        scanLine = (ImageView) findViewById(R.id.capture_scan_line);
+        ivBack = (ImageView) findViewById(R.id.iv_back);
+        mScanDescribeView = (TextView) findViewById(R.id.capture_mask_bottom);
+
+        if(!TextUtils.isEmpty(mTitle)){
+            mTitleView.setText(mTitle);
+        }
+        if(!TextUtils.isEmpty(mScanDescribe)){
+            mScanDescribeView.setText(mScanDescribe);
+        }
+        mTitleView.setTextColor(mTitleColor);
+        mScanDescribeView.setTextColor(mScanDescColor);
+        mTitleBarView.setBackgroundColor(mTitleBgColor);
+
+        scanCropView.setBackground(getTintDrawable(R.drawable.zxing_code_bg, mScanColor));
+        scanLine.setBackground(getTintDrawable(R.drawable.zxing_scan_line, mScanColor));
+
+
+    }
+
+    private Drawable getTintDrawable(int resId, int color){
+        Drawable original = ContextCompat.getDrawable(this, resId);
+        Drawable wrappedDrawable = DrawableCompat.wrap(original);
+        DrawableCompat.setTint(wrappedDrawable, color);
+        return wrappedDrawable;
     }
 
     @Override
@@ -151,6 +199,7 @@ public class CaptureActivity extends Activity implements
         }
 
         inactivityTimer.onResume();
+
     }
 
     @Override
@@ -203,18 +252,16 @@ public class CaptureActivity extends Activity implements
      */
     public void handleDecode(Result rawResult, Bundle bundle) {
         inactivityTimer.onActivity();
-//        beepManager.playBeepSoundAndVibrate();
-        if (captureType == 0) {
-            Intent resultIntent = new Intent();
-            bundle.putInt("width", mCropRect.width());
-            bundle.putInt("height", mCropRect.height());
-            bundle.putString("result", rawResult.getText());
-            resultIntent.putExtras(bundle);
-            this.setResult(RESULT_OK, resultIntent);
-            CaptureActivity.this.finish();
-        } else {
-            scanDeviceSuccess(rawResult.toString(), bundle);
+        if(mNeedBeep){
+            beepManager.playBeepSoundAndVibrate();
         }
+        Intent resultIntent = new Intent();
+        bundle.putInt("width", mCropRect.width());
+        bundle.putInt("height", mCropRect.height());
+        bundle.putString("result", rawResult.getText());
+        resultIntent.putExtras(bundle);
+        this.setResult(RESULT_OK, resultIntent);
+        CaptureActivity.this.finish();
 
     }
 
@@ -264,9 +311,9 @@ public class CaptureActivity extends Activity implements
     private void displayFrameworkBugMessageAndExit() {
         // camera error
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(getString(R.string.jwstr_prompt));
-        builder.setMessage(getString(R.string.jwstr_camera_error));
-        builder.setPositiveButton(getString(R.string.jwstr_confirm), new DialogInterface.OnClickListener() {
+        builder.setTitle(getString(R.string.zxing_prompt));
+        builder.setMessage(getString(R.string.zxing_camera_error));
+        builder.setPositiveButton(getString(R.string.zxing_confirm), new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -286,7 +333,7 @@ public class CaptureActivity extends Activity implements
 
     public void restartPreviewAfterDelay(long delayMS) {
         if (handler != null) {
-            handler.sendEmptyMessageDelayed(R.id.jwstr_restart_preview, delayMS);
+            handler.sendEmptyMessageDelayed(R.id.zxing_restart_preview, delayMS);
         }
     }
 
@@ -350,42 +397,6 @@ public class CaptureActivity extends Activity implements
                 this.setResult(RESULT_CANCELED, resultIntent);
                 CaptureActivity.this.finish();
                 break;
-            case 124:
-                if (captureType == 0) {
-                    Intent mullt = new Intent();
-                    this.setResult(RESULT_MULLT, mullt);
-                    CaptureActivity.this.finish();
-                } else {
-//                    Intent add = new Intent(this, AddContactActivity.class);
-//                    CaptureActivity.this.startActivity(add);
-//                    CaptureActivity.this.finish();
-                    Intent resultInten1t = new Intent();
-                    this.setResult(RESULT_CANCELED, resultInten1t);
-                    CaptureActivity.this.finish();
-                }
-                break;
-            case 125:
-                //打开相册选择图片
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(intent, CODE_GALLERY_REQUEST);
-                break;
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == CODE_GALLERY_REQUEST) {
-            String picPath = SelectAlbumUtils.getPicPath(this, data);
-            Result result = DecodeBitmap.scanningImage(picPath);
-            if (result == null) {
-                Toast.makeText(this, getString(R.string.jwstr_pic_no_qrcode), Toast.LENGTH_SHORT).show();
-            } else {
-//                beepManager.playBeepSoundAndVibrate();
-                String scanResult = DecodeBitmap.parseReuslt(result.toString());
-                scanDeviceSuccess(scanResult, new Bundle());
-            }
         }
     }
 
